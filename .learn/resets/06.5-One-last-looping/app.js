42492e39a6d712fef9c9c9f16aad4fd1053d3d29
Luis Piñero Quintana <lpineroq@gmail.com>
2022-01-24 19:22:02 +0000

    Hasta el ejercicio 6.5

--- .learn/resets/06.5-One-last-looping/app.js
+++ .learn/resets/06.5-One-last-looping/app.js
@@ -1,6 +1,3 @@
 let mySampleArray = ['Esmeralda', 'Kiko', 'Ruth', 'Lebron', 'Pedro', 'Maria', 'Lou', 'Fernando', 'Cesco', 'Bart', 'Annie'];
 
-for (let i = mySampleArray.length - 1 ; i >= 0; i--)
-{
-    console.log(myaSampleArray[i])
-}
+//your code here
